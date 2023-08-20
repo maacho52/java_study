825aeb04ea905cfdc126662d815f6bfee231257e
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-08-20 04:29:39 +0000

    [Create]オブジェクト指向

---
+++ chap09/calcWithConstructor.js
@@ -0,0 +1,22 @@
+//たろうさん用のTestScoreWithConstructorを使って、データを表示
+let taro = new TestScoreWithConstrctor("たろう", 92, 87, 74);
+taro.printScore();
+//はなこさん用のTestScoreWithConstructorを使って、データを表示
+let hanako = new TestScoreWithConstrctor("はなこ", 79, 95, 83);
+hanako.printScore();
+
+//たろうさんの合計点を取得
+let taroSum = taro.calcSum();
+//はなこさんの合計点を取得
+let hanakoSum = hanako.calcSum();
+//二人の合計の平均点を算出し、表示
+let ave2 = (taroSum + hanakoSum) / 2;
+console.log("二人の合計の平均: " + ave2);
+
+//たろうさんの平均点を取得
+let taroAve = taro.calcAve();
+//はなこさんの平均点を取得
+let hanakoAve = hanako.calcAve();
+//二人の平均の平均点を算出し、取得
+let aveAve = (taroAve + hanakoAve) / 2;
+console.log("二人の平均の平均: " + aveAve);
